ad2adff9d5c1c91df4ca1bd497538717c4c8209d
DCommicsBP <dayonne.p@gmail.com>
2018-09-20 20:53:47 -0300

    login web done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,18 @@ import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { IntroPageModule } from '../pages/intro/intro.module';
 import { LoginPageModule } from '../pages/login/login.module';
 import { MapsPageModule } from '../pages/maps/maps.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { GooglePlus } from '@ionic-native/google-plus';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAA-CoZcaF2tT2DKPCTnQPepP2tgIoSSQ",
+  authDomain: "mypoopyproject-198f6.firebaseapp.com",
+  databaseURL: "https://mypoopyproject-198f6.firebaseio.com",
+  projectId: "mypoopyproject-198f6",
+  storageBucket: "mypoopyproject-198f6.appspot.com",
+  messagingSenderId: "192689498859"
+}
 
 
 @NgModule({
@@ -19,6 +31,8 @@ import { MapsPageModule } from '../pages/maps/maps.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
     IntroPageModule, 
     LoginPageModule,
     MapsPageModule
@@ -29,6 +43,7 @@ import { MapsPageModule } from '../pages/maps/maps.module';
   ],
   providers: [
     StatusBar,
+    GooglePlus,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AndroidPermissions,
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MapsPage } from '../maps/maps';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Platform } from 'ionic-angular'; 
 
 @IonicPage()
 @Component({
@@ -16,11 +15,59 @@ import { MapsPage } from '../maps/maps';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: Observable<firebase.User>;
+
+
+  constructor(public navCtrl: NavController, 
+          public navParams: NavParams,
+          private afAuth: AngularFireAuth,
+          private gplus: GooglePlus,
+          private platform: Platform) {
+            this.user = this.afAuth.authState;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  googleLogin() {
+    if (this.platform.is('cordova')) {
+      this.nativeGoogleLogin();
+    } else {
+        this.webGoogleLogin();
+    }
+  }
+
+  async nativeGoogleLogin(): Promise<any> {
+    try {
+      const gplusUser = await this.gplus.login({
+        'webClientId': '192689498859-66libcec4t10p7m49igtn9lqg9f11tco.apps.googleusercontent.com',
+        'offline': true
+      });
+
+      return await this.afAuth.auth.signInWithCredential(
+        firebase.auth.GoogleAuthProvider.credential(gplusUser.isToken)
+      );
+
+    } catch (error) {
+      console.log('Error => ', error);
+    }
+  }
+
+
+  async webGoogleLogin(): Promise<any> {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider);
+
+      console.log('CREDENTIALS ==> ', credential);
+    } catch (error) {
+      console.log('Error => ', error);
+    }
+  }
+
+
+  signOut() {
+    this.afAuth.auth.signOut();
+    if (this.platform.is('cordova')) {
+      this.gplus.logout();
+    }
   }
 
   goToTabsPage() {
