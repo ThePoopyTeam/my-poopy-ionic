f6a602b2a321da2d6b4c256d437fd92bf471604a
Priscila Caimi <priscila.caimi@hogarthww.com>
2018-10-16 14:51:15 -0300

    adicionando ações nos botões do menu e menu rapido

--- src/pages/cadastro-banheiro/cadastro-banheiro.ts
+++ src/pages/cadastro-banheiro/cadastro-banheiro.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, Platform } from 'ionic-angular';
 import { HttpClient,} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable'
+
+import { MapsPage } from '../maps/maps';
+
 /**
  * Generated class for the CadastroBanheiroPage page.
  *
@@ -18,10 +21,18 @@ export class CadastroBanheiroPage {
 
   public catacteristicas: any;
 
+  rootPage:any = MapsPage;
+  homePage:any;
+  cadastroPage:any;
+
   constructor(public navCtrl: NavController, public httpClient: HttpClient, 
     public Platform: Platform) {
 
     this.getData();
+    // Ações No Menu - side bar
+    this.homePage = MapsPage;
+    this.cadastroPage = CadastroBanheiroPage;
+
   }
 
   getData(){
@@ -50,5 +61,15 @@ export class CadastroBanheiroPage {
       infiniteScroll.complete();
     }, 500);
   }
+
+  // vai usar o proprio botão de voltar do celular
+  goBack() {
+    this.navCtrl.pop();
+  }
+
+  openPage(opcao) {
+    this.rootPage = opcao;
+  }
+
 
 }// fim da classe
