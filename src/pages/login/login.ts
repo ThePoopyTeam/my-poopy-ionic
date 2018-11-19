22b28355a9bfcfd517a61ab83fdd61f3397137f3
Priscila Caimi <priscila.caimi@hogarthww.com>
2018-11-19 20:12:44 -0200

    integracao com o google plus

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController  } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { MapsPage } from '../maps/maps';
 import { Facebook } from '@ionic-native/facebook';
 
@@ -35,17 +35,17 @@ export class LoginPage {
   user: Observable<firebase.User>;
 
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public facebook: Facebook, 
-    private userProvider: UsersProvider, 
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public facebook: Facebook,
+    private userProvider: UsersProvider,
     private toast: ToastController,
     public storage: Storage,
     //login google
     private afAuth: AngularFireAuth,
-    public googleplus: GooglePlus, 
+    private gplus: GooglePlus,
     private platform: Platform) {
-    
+
     this.storage.get('uid').then(done => {
       if (!done) {
 
@@ -61,7 +61,7 @@ export class LoginPage {
     this.user = this.afAuth.authState;
 
   }
-  
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
@@ -82,7 +82,7 @@ export class LoginPage {
     this.navCtrl.push(MapsPage);
   }
 
-  fbLogin(){
+  fbLogin() {
     this.facebook.login(['email']).then(res => {
       const fc = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
       firebase.auth().signInWithCredential(fc).then(fs => {
@@ -127,85 +127,112 @@ export class LoginPage {
         this.toast.create({ message: 'Erro ao logar. Tente novamente!!!', position: 'botton', duration: 3000 }).present();
         console.log(ferr)
       })
-    }).catch(err =>{
+    }).catch(err => {
       console.log(err)
     })
   }
 
+  /*
+  * LOGIN COM O GMAIL E LOGOUT
+  */
+
   googleLogin2() {
-    if(this.platform.is('cordova')) {
+    if (this.platform.is('cordova')) {
       this.nativeGoogleLogin();
-    }else {
+    } else {
       this.webGoogleLogin();
     }
   }
 
   async nativeGoogleLogin(): Promise<void> {
-    try{
+    try {
+
       const gplusUser = await this.gplus.login({
         'webClientId': '192689498859-66libcec4t10p7m49igtn9lqg9f11tco.apps.googleusercontent.com',
         'offline': true,
         'scopes': 'profile email'
       })
 
-      return await this.afAuth.auth.signInWithCredential(
-        firebase.auth.GoogleAuthProvider.credential()
-      )
+      await
+        this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken))
+
+    } catch (err) {
+      console.log(err)
     }
   }
 
-  googleLogin(){
-    console.log('apertou botão google')
-    this.googleplus.login({
-      'webClientId': '192689498859-66libcec4t10p7m49igtn9lqg9f11tco.apps.googleusercontent.com',
-      'offline' : true
-    }).then(res => {
-      const gc = firebase.auth.GoogleAuthProvider.credential(res.idToken);
-      console.log('Log 1: ' + gc)
-      firebase.auth().signInWithCredential(gc).then(suc => {
-        this.toast.create({ message: 'Usuário logado com sucesso. ', position: 'botton', duration: 3000 }).present();
 
-        this.storage.set('uid', true);
-        this.storage.set('uidNumber', suc.uid.toString());
-        this.navCtrl.push(MapsPage);
-        /*
-     
-        this.model = new User();
-        console.log(suc)
-        this.model.nome = suc.displayName.toString()
-        this.model.email = suc.providerData[0].email.toString()
-        this.model.imagem = suc.photoURL.toString()
-        this.model.experiencia = 0
-        this.model.uid = suc.uid.toString()
+  async webGoogleLogin(): Promise<void> {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider);
 
-        //chama provider
-        this.userProvider.createAccount(this.model.nome, this.model.email, this.model.imagem, this.model.experiencia, this.model.uid)
-          .then((result: any) => {
-            this.toast.create({ message: 'Usuário logado com sucesso. ', position: 'botton', duration: 3000 }).present();
+    } catch (err) {
+      console.log(err)
+    }
 
-            this.storage.set('uid', true);
-            this.storage.set('uidNumber', this.model.uid);
-
-            //Salvar o token no Ionic Storage para usar em futuras requisições.
-            //Redirecionar o usuario para outra tela usando o navCtrl
-            //this.navCtrl.pop();
-            //this.navCtrl.setRoot()
-            this.navCtrl.push(MapsPage);
-          })
-          .catch((error: any) => {
-            this.toast.create({ message: 'Erro ao logar. Tente novamente!!!', position: 'botton', duration: 3000 }).present();
-            console.log(error.error)
-          });
-
-          */
-      }).catch(ns => {
-        this.toast.create({ message: 'Erro ao logar. Tente novamente!!!', position: 'botton', duration: 3000 }).present();
-        console.log(ns)
-      })
-      }).catch(err => {
-        console.log(err)
-      })
   }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+    if (this.platform.is('cordova')) {
+      this.gplus.logout();
+    }
+  }
+
+
+  // googleLogin(){
+  //   console.log('apertou botão google')
+  //   this.googleplus.login({
+  //     'webClientId': '192689498859-66libcec4t10p7m49igtn9lqg9f11tco.apps.googleusercontent.com',
+  //     'offline': true
+  //   }).then(res => {
+  //     const gc = firebase.auth.GoogleAuthProvider.credential(res.idToken);
+  //     console.log('Log 1: ' + gc)
+  //     firebase.auth().signInWithCredential(gc).then(suc => {
+  //       this.toast.create({ message: 'Usuário logado com sucesso. ', position: 'botton', duration: 3000 }).present();
+
+  //       this.storage.set('uid', true);
+  //       this.storage.set('uidNumber', suc.uid.toString());
+  //       this.navCtrl.push(MapsPage);
+  //       /*
+
+  //       this.model = new User();
+  //       console.log(suc)
+  //       this.model.nome = suc.displayName.toString()
+  //       this.model.email = suc.providerData[0].email.toString()
+  //       this.model.imagem = suc.photoURL.toString()
+  //       this.model.experiencia = 0
+  //       this.model.uid = suc.uid.toString()
+
+  //       //chama provider
+  //       this.userProvider.createAccount(this.model.nome, this.model.email, this.model.imagem, this.model.experiencia, this.model.uid)
+  //         .then((result: any) => {
+  //           this.toast.create({ message: 'Usuário logado com sucesso. ', position: 'botton', duration: 3000 }).present();
+
+  //           this.storage.set('uid', true);
+  //           this.storage.set('uidNumber', this.model.uid);
+
+  //           //Salvar o token no Ionic Storage para usar em futuras requisições.
+  //           //Redirecionar o usuario para outra tela usando o navCtrl
+  //           //this.navCtrl.pop();
+  //           //this.navCtrl.setRoot()
+  //           this.navCtrl.push(MapsPage);
+  //         })
+  //         .catch((error: any) => {
+  //           this.toast.create({ message: 'Erro ao logar. Tente novamente!!!', position: 'botton', duration: 3000 }).present();
+  //           console.log(error.error)
+  //         });
+
+  //         */
+  //     }).catch(ns => {
+  //       this.toast.create({ message: 'Erro ao logar. Tente novamente!!!', position: 'botton', duration: 3000 }).present();
+  //       console.log(ns)
+  //     })
+  //   }).catch(err => {
+  //     console.log(err)
+  //   })
+  // }
 }
 
 export class User {
@@ -213,5 +240,5 @@ export class User {
   email: String;
   imagem: String;
   experiencia: Number;
-  uid:String;
+  uid: String;
 }
